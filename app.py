7eef06f94c35c6ab92193fd6658a1d4920e96b2d
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-09-26 17:08:15 +0100

    calculate financials removed from app.py and moved to calc current values

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import time
 
 from stock_data import get_stock_history
 from data_processing import process_transactions
+from financial_calculations import calculate_current_values
 
 st.set_page_config(page_title="Investment Portfolio", page_icon="logo.svg")
 
@@ -22,51 +23,6 @@ def load_transactions(json_file_path):
     with open(json_file_path, 'r') as file:
         transactions_data = json.load(file)
     return pd.DataFrame(transactions_data)
-
-def calculate_current_values(holdings, transactions_df):
-    """Calculate current values and profit/loss per stock."""
-    # --- Calculate Current Values and Total Portfolio Value ---
-    current_values = {}
-    profit_loss_per_stock = {}
-    total_current_value = 0.0
-    total_invested_amount = 0.0
-
-    for ticker, shares in holdings.items():
-        ticker_obj = yf.Ticker(ticker)
-        try:
-            # Attempt to get the current price
-            current_price = ticker_obj.history(period='1d')['Close'][0]
-
-            current_value = current_price * shares
-            current_values[ticker] = current_value
-            total_current_value += current_value
-
-            # Calculate total invested amount for this stock
-            total_invested = 0.0
-            for idx, transaction in transactions_df[transactions_df['Ticker Symbol'] == ticker].iterrows():
-                shares_transacted = float(transaction["No. of Shares"])
-                avg_cost = float(transaction["Average Cost per Share USD"])
-                total_cost = shares_transacted * avg_cost
-                if transaction["Transaction Type"] == "BUY":
-                    total_invested += total_cost
-                elif transaction["Transaction Type"] == "SELL":
-                    total_invested -= total_cost
-
-            total_invested_amount += total_invested
-
-            # Calculate profit or loss for this stock
-            profit_loss = current_value - total_invested
-            profit_loss_per_stock[ticker] = profit_loss
-
-        except Exception as e:
-            current_values[ticker] = 0.0
-            st.error(f"Failed to retrieve price for {ticker}: {e}")
-
-    # Calculate total profit or loss
-    total_profit_loss = total_current_value - total_invested_amount
-
-    return current_values, profit_loss_per_stock, total_current_value, total_invested_amount, total_profit_loss
-
 
 def display_overall_holdings(total_current_value, total_invested_amount, total_profit_loss):
     """Display overall holdings at the top."""
